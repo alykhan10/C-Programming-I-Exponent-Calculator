66726083220c228f17db93c0935d00c17917c469
Aly Khan Nuruddin <76642945+alykhan10@users.noreply.github.com>
2021-06-11 19:36:17 +0500

    Update base_exponent.c

--- base_exponent.c
+++ base_exponent.c
@@ -1,5 +1,5 @@
 /*****************************************************************************************************************/
-/*													Exponent Calculator											 */
+/*						  Exponent Calculator						 */
 /*****************************************************************************************************************/
 
 /*
@@ -12,58 +12,58 @@
 
 /*****************************************************************************************************************/
  
-#define _CRT_SECURE_NO_WARNINGS														// Prohibit the compiler from suggesting secure versions of library functions.
+#define _CRT_SECURE_NO_WARNINGS									// Prohibit the compiler from suggesting secure versions of library functions.
 
-#include <stdio.h>																	// Include the standard input and output library in C.
-#include <stdlib.h>																	// Include the standard library in C.
+#include <stdio.h>										// Include the standard input and output library in C.
+#include <stdlib.h>										// Include the standard library in C.
  
  int main( void )
  {   
-	 int count = 1;																	// Initialize "count" as a variable that keeps track of each loop iteration.
+	 int count = 1;										// Initialize "count" as a variable that keeps track of each loop iteration.
 
-	 int exponent;																	// Define "exponent" as a variable that stores the number of times the base is multiplied.
+	 int exponent;										// Define "exponent" as a variable that stores the number of times the base is multiplied.
      
-	 float base;																	// Define "base" as a variable that stores the value of the number being multiplied.
-     float answer;																	// Define "answer" as a variable that stores the result of the mathematical operation.
+	 float base;										// Define "base" as a variable that stores the value of the number being multiplied.
+     float answer;										// Define "answer" as a variable that stores the result of the mathematical operation.
      
-     printf( "Enter the value of the base: " );										// Prompt the user to enter the value of the base.
-     scanf( "%f", &base );															// Read the value entered by the user and assign it to the variable "base".
+     printf( "Enter the value of the base: " );							// Prompt the user to enter the value of the base.
+     scanf( "%f", &base );									// Read the value entered by the user and assign it to the variable "base".
      
-     while ( base < 0 )																// Define the condition where the user responds with a negative value.
+     while ( base < 0 )										// Define the condition where the user responds with a negative value.
      {
          printf( "\nError: The value of the base cannot be negative!\n" );			// Notify the user that they have entered an invalid input.
-         printf( "\nEnter the value of the base: " );								// Prompt the user to re-enter the value of the base.
-         scanf( "%f", &base );														// Read the value entered by the user and assign it to the variable "base".
+         printf( "\nEnter the value of the base: " );						// Prompt the user to re-enter the value of the base.
+         scanf( "%f", &base );									// Read the value entered by the user and assign it to the variable "base".
      }
      
-	 printf( "\nEnter the value of the exponent: " );								// Prompt the user to enter the value of the exponent.
-	 scanf( "%d", &exponent );														// Read the value entered by the user and assign it to the variable "exponent".
+	 printf( "\nEnter the value of the exponent: " );					// Prompt the user to enter the value of the exponent.
+	 scanf( "%d", &exponent );								// Read the value entered by the user and assign it to the variable "exponent".
 
-	 while (exponent < 0)															// Define the condition where the user responds with a negative value.
+	 while (exponent < 0)									// Define the condition where the user responds with a negative value.
 	 {
 		 printf( "\nError: The value of the exponent cannot be negative!\n" );		// Notify the user that they have entered an invalid input.
-		 printf( "\nEnter the value of the exponent: ");							// Prompt the user to re-enter the value of the exponent.
-		 scanf( "%d", &exponent );													// Read the value entered by the user and assign it to the variable "exponent".
+		 printf( "\nEnter the value of the exponent: ");				// Prompt the user to re-enter the value of the exponent.
+		 scanf( "%d", &exponent );							// Read the value entered by the user and assign it to the variable "exponent".
 	 }
 
-     if ( exponent == 0 )															// Define the condition where the user enters '0' for the exponent.
+     if ( exponent == 0 )									// Define the condition where the user enters '0' for the exponent.
      {
-         answer = 1;																// Assign the value of "answer" to be '1'.
+         answer = 1;										// Assign the value of "answer" to be '1'.
      }
      
-     else																			// Define the condition where the user enters any number other than '0' for the exponent.
+     else											// Define the condition where the user enters any number other than '0' for the exponent.
      {
-         answer = base;																// Assign the "answer" to preliminarily equal the value of "base".
+         answer = base;										// Assign the "answer" to preliminarily equal the value of "base".
      
-         while ( count < exponent )													// Define the condition where the value of "count" is less than the value of the "exponent".
+         while ( count < exponent )								// Define the condition where the value of "count" is less than the value of the "exponent".
          {   
-             answer *= base;														// Assign the value of "answer" to be the product of the previous value of "answer" and "base".
-             count++;																// Increment the value of "count" by 1.
+             answer *= base;									// Assign the value of "answer" to be the product of the previous value of "answer" and "base".
+             count++;										// Increment the value of "count" by 1.
          }
      }
      
-     printf( "\nAnswer: %f ^ %d = %.3f.", base, exponent, answer);					// Print the value of the "base" raised to the power of the "exponent" to three decimal places.
+     printf( "\nAnswer: %f ^ %d = %.3f.", base, exponent, answer);				// Print the value of the "base" raised to the power of the "exponent" to three decimal places.
      
-	 system("PAUSE");																// Suspend the execution of the function "main".
-	 return 0;																		// Terminate the execution of the function "main".
+	 system("PAUSE");									// Suspend the execution of the function "main".
+	 return 0;										// Terminate the execution of the function "main".
  }
